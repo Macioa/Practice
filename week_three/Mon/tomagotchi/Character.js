cff2e3eb75770acb883f785959ac0cde74787c15
Ryan Montgomery <ryan.maciomaire@gmail.com>
2018-06-27 12:40:47 -0500

    spring cleaning

--- week_three/Mon/tomagotchi/Character.js
+++ week_three/Mon/tomagotchi/Character.js
@@ -1,4 +1,4 @@
-const hungerInterval = 60; //how often hunger decreases in seconds
+const hungerInterval = 1; //how often hunger decreases in seconds
 const sleepinessInterval = 60; //how often sleepiness decreases in seconds
 const boredomInterval = 60; //how often boredom decreases in seconds
 const animationInterval = 5; 
@@ -7,13 +7,14 @@ const growthInterval = 5; //how often size increases, by age
 const growthIncrement = .25; //how much char grows in each increment (relative to default size set in css)
 const maxStat = 12; //maximum value for hunger/sleepiness/boredom
 const travelIncrement = 25; //number of pixels to travel each increment
+
+const debug=true;
  
 const getImgRelativeSize = (filename) => {
     let newimg = document.createElement('img');
     newimg.src=(`img/${filename}`);
     let width = newimg.naturalWidth;
     let height = newimg.naturalHeight;
-    console.log((width+height)/2);
     return ((width+height)/2);
 }
 
@@ -87,9 +88,13 @@ class Tomagotchi {
     }
 
     tick(){
+        if (!this.alive)
+            return;
+
         this.travel();
-        console.log(this.tickCount);
+
         this.tickCount++;
+
         if (this.hunger<5)
             this.emotion="hungry";
         else if (this.sleepiness<5)
@@ -101,22 +106,31 @@ class Tomagotchi {
             this.emotion = "dead";
             this.alive = false;
             console.log(`${this.name} has died.`);
-            //this.renderEmotion();
         }
 
         if (this.tickCount%hungerInterval===0)
-            this.hunger-=1;
+            if (!Math.floor(Math.random()*3))
+                    this.hunger-=1;
         if (this.tickCount%boredomInterval===0)
-            this.boredom-=1;
+            if (!Math.floor(Math.random()*3))
+                this.boredom-=1;
         if (this.tickCount%sleepinessInterval===0)
-            this.sleepiness-=1;
+            if (!Math.floor(Math.random()*3))    
+                this.sleepiness-=1;
+
         if (this.tickCount%ageInterval===0)
             this.age+=1;
         if (this.tickCount%growthInterval===0)
-            this.scale+=growthIncrement;
+            if (!Math.floor(Math.random()*3))
+                this.scale+=growthIncrement;
+
+        if (this.tick===1000)
+            this.tick=0;
 
         //this.renderEmotion();
         //this.scaleRender();
+        if (debug)
+            console.log(`${this.name} has ${this.hunger} hunger, ${this.sleepiness} sleepiness, and ${this.boredom} boredom. ${this.name} is ${this.emotion}.`);
     }
     renderEmotion(){
         switch (this.emotion){
@@ -156,6 +170,7 @@ class Tomagotchi {
             this.sleepiness=maxStat;
     }
     travel(){
+        // check window bounds against html element position to determine travel direction
         if (this.htmlelement.getBoundingClientRect().right>window.innerWidth )
             this.xTravel=-travelIncrement;
         if (this.htmlelement.getBoundingClientRect().bottom>window.innerHeight )
@@ -169,7 +184,6 @@ class Tomagotchi {
         let ypos = parseInt(this.htmlelement.style.top.replace("px",""));
         this.htmlelement.style.left=xpos+this.xTravel+"px";
         this.htmlelement.style.top=ypos+this.yTravel+"px";
-        console.log(this.htmlelement.style.top)
     }
 }
 
