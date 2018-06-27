1f25cea285f6bdeb6cdb670a67d844085c7b71da
Ryan Montgomery <ryan.maciomaire@gmail.com>
2018-06-27 15:23:11 -0500

    completed js for menu. need to integrate with character.

--- week_three/Wed/MenuMock/menu.js
+++ week_three/Wed/MenuMock/menu.js
@@ -1,12 +1,13 @@
+
+
 class menu {
-    constructor(parentCharacter){ 
-        this.parentCharacter=parentCharacter;
-        if (this.parentCharacter)
-            this.name=this.parentCharacter.name;
-        else {
-            console.error('No parent for menu');
-            return;
-        }   
+    constructor(name){ 
+        this.name=name;
+        this.constructHtml(); 
+        this.canPlay=true;
+        this.canSleep=true;
+        this.canEat=true;
+        this.onmouseexit();
     }
     constructHtml(){
         this.htmlelement = document.createElement('div');
@@ -14,8 +15,8 @@ class menu {
         this.htmlelement.id=`${this.name}menuContainer`;
 
         this.accessButton = document.createElement('div');
-        this.accessButton.className = "accessContainer";
-        this.accessButton.id=`${this.name}accessContainer`;
+        this.accessButton.className = "accessButton";
+        this.accessButton.id=`${this.name}accessButton`;
         this.accessButton.innerHTML= "&#x2630";
 
         this.playButton = document.createElement('div');
@@ -34,8 +35,57 @@ class menu {
         this.eatButton.style.backgroundImage = "url('img/icon/eat.png')";
 
         this.htmlelement.append(this.accessButton);
+        this.htmlelement.append(document.createElement('br'));
         this.htmlelement.append(this.playButton);
         this.htmlelement.append(this.sleepButton);
         this.htmlelement.append(this.eatButton);
+
+        let me = this;
+        this.htmlelement.addEventListener("mouseenter", function(event) {me.onmouseover();} );
+        this.htmlelement.addEventListener("mouseleave", function(event) {me.onmouseexit();} );
+        this.playButton.addEventListener("click", function(event) {me.playClick();} );
+        this.eatButton.addEventListener("click", function(event) {me.eatClick();} );
+        this.sleepButton.addEventListener("click", function(event) {me.sleepClick();} );
         }
+    onmouseover(){
+        if (this.canPlay)
+            this.playButton.style.visibility="visible";
+        if (this.canEat)
+            this.eatButton.style.visibility="visible";
+        if (this.canSleep)
+            this.sleepButton.style.visibility="visible";
+    }
+    onmouseexit(){
+        this.playButton.style.visibility="hidden";
+        this.eatButton.style.visibility="hidden";
+        this.sleepButton.style.visibility="hidden";
+    }
+    attach(parentHtml, top=0){
+        parentHtml.append(this.htmlelement);
+        this.htmlelement.style.top=top+"px";
+        
+        this.htmlelement.style.left=parentHtml.offsetWidth/2-this.htmlelement.offsetWidth/2+"px";
+    }
+    playClick(){
+        //this.parentCharacter.play();
+        this.canPlay=false;
+        this.playButton.style.visibility="hidden";
+    }
+    eatClick(){
+        //this.parentCharcater.eat();
+        this.canEat=false;
+        this.eatButton.style.visibility="hidden";
+    }
+    sleepClick(){
+        //this.parentCharacter.sleep();
+        this.canSleep=false;
+        this.sleepButton.style.visibility="hidden";
+    }
 }
+
+
+var mymenu = new menu("tim");
+mymenu.attach(document.getElementsByTagName('body')[0]);
+mymenu.playButton.style.visibility="hidden";
+var mymenu = new menu("bob");
+mymenu.attach(document.getElementsByTagName('body')[0]);
