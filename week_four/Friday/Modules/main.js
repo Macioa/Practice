9da2f29be4316ae5a49e8038233629b3c7b28526
Ryan Montgomery <ryan.maciomaire@gmail.com>
2018-07-07 10:43:33 -0500

    commit from fri

---
+++ week_four/Friday/Modules/main.js
@@ -0,0 +1,8 @@
+//examples of modules
+/*
+express - server side framework
+-parse http requests
+-organize code in an mvc structure*/
+
+const my = require('./myModule');
+console.log(my);
---
+++ week_four/Friday/Modules/myModule.js
@@ -0,0 +1,8 @@
+const number = 7;
+module.exports.name = 'myName';
+module.exports.someArr = [1,2,3];
+module.exports.getNumber = () => {
+    console.log('Get number called: ', number)
+}
+
+console.log('eof');
