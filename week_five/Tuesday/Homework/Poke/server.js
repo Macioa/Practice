cf46a5b80759f68c72c85578c5e68ef8a3f999c0
Ryan Montgomery <ryan.maciomaire@gmail.com>
2018-07-11 10:53:07 -0500

    prelab

--- week_five/Tuesday/Homework/Poke/server.js
+++ week_five/Tuesday/Homework/Poke/server.js
@@ -2,11 +2,15 @@ const express = require('express');
 var port = 3000;
 const server = express();
 
+//
 
+var pokeList = require('./model/pokemon');
+console.log(pokeList)
 //server get, url string, request, response
 
 server.get('/pokemon/', (request, response) =>{
-    response.render('index.ejs');
+    response.send(pokeList);
+    response.render('index.ejs', pokeList);
 })
 
-server.listen(port, function(){console.log(`server listening on ${port}`)})
+server.listen(port, function(){ console.log(`server listening on ${port}`) })
