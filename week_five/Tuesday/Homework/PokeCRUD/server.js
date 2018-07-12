b7c09205c6f72d145bb27b6feaac8f81aa121c7d
Ryan Montgomery <ryan.maciomaire@gmail.com>
2018-07-11 20:30:18 -0500

    crud complete

--- week_five/Tuesday/Homework/PokeCRUD/server.js
+++ week_five/Tuesday/Homework/PokeCRUD/server.js
@@ -11,6 +11,9 @@ console.log(pokeList)
 
 
 //Create
+server.put('/pokemone/create', (request, response) =>{
+    pokeList.push(request.body);
+})
 
 //Read
 server.get('/pokemon/', (request, response) =>{
@@ -22,11 +25,14 @@ server.get('/pokemon/:index', (request, response) =>{
 });
 
 //Update
+server.put('/pokemon/:index', (request, response) =>{
+    pokeList[request.params.index] = request.body;
+});
 
 //Delete
 server.delete('/pokemon/:index'), (request, response) =>{
     pokeList = pokeList.splice(request.params.index, 1);
-    server.redirect('/pokemon/')
+    server.redirect('/pokemon/');
 }
 
 
