6bdd37f323923f8ad9a76b5ce7c141314c5fdd1d
Ryan Montgomery <ryan.maciomaire@gmail.com>
2018-07-17 18:57:47 -0500

    put route in articles. last of https://git.generalassemb.ly/WebDev-Connected-Classroom/two-model-relationship-build

--- week_six/Blog/Controllers/articles.js
+++ week_six/Blog/Controllers/articles.js
@@ -54,23 +54,41 @@ router.get('/:id', (req, res)=>{
 });
 
 
-router.get('/:id/edit', (req, res) => {
-  Article.findById(req.params.id, (err, foundArticle) => {
-    res.render('articles/edit.ejs', {
-      article: foundArtcile
-    })
-  })
-})
+router.get('/:id/edit', (req, res)=>{
+	Article.findById(req.params.id, (err, foundArticle)=>{
+		Author.find({}, (err, allAuthors)=>{
+			Author.findOne({'articles._id':req.params.id}, (err, foundArticleAuthor)=>{
+				res.render('articles/edit.ejs', {
+					article: foundArticle,
+					authors: allAuthors,
+					articleAuthor: foundArticleAuthor
+				});
+			});
+		});
+	});
+});
 
 
 router.put('/:id', (req, res)=>{
     Article.findByIdAndUpdate(req.params.id, req.body, { new: true }, (err, updatedArticle)=>{
         Author.findOne({ 'articles._id' : req.params.id }, (err, foundAuthor)=>{
-            foundAuthor.articles.id(req.params.id).remove();
-            foundAuthor.articles.push(updatedArticle);
-            foundAuthor.save((err, data)=>{
-                res.redirect('/articles/'+req.params.id);
-            });
+		if(foundAuthor._id.toString() !== req.body.authorId){
+			foundAuthor.articles.id(req.params.id).remove();
+			foundAuthor.save((err, savedFoundAuthor)=>{
+				Author.findById(req.body.authorId, (err, newAuthor)=>{
+					newAuthor.articles.push(updatedArticle);
+					newAuthor.save((err, savedNewAuthor)=>{
+			        	        res.redirect('/articles/'+req.params.id);
+					});
+				});
+			});
+		} else {
+			foundAuthor.articles.id(req.params.id).remove();
+			foundAuthor.articles.push(updatedArticle);
+			foundAuthor.save((err, data)=>{
+		                res.redirect('/articles/'+req.params.id);
+			});
+		}
         });
     });
 });
